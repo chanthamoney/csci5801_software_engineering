46be7be2a9f936d02f56e11a1fc611a82b29dc58
Meghann Silagan <silag001@umn.edu>
2018-11-04 23:46:41 -0600

    finished TestIRV, may need to add more test cases

--- Project1/src/TestIRV.java
+++ Project1/src/TestIRV.java
@@ -6,19 +6,19 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.IOException;
+import java.io.*;
 
 public class TestIRV {
-  ArrayList<ArrayList<Integer>> testBallots;
+  ArrayList<ArrayList<Integer>> testBallots = new ArrayList<ArrayList<Integer>>();
   String[] candidates = {"Naruto", "Sasuke", "Sakura", "Kakashi"};
 
   private IRV intitializeTestIRV() {
-    ArrayList<Integer> firstBallot = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-    ArrayList<Integer> secondBallot = new ArrayList<>(Arrays.asList(3, 1, 2, 4));
-    ArrayList<Integer> thirdBallot = new ArrayList<>(Arrays.asList(2, 1, 3, 4));
-    ArrayList<Integer> fourthBallot = new ArrayList<>(Arrays.asList(4, 3, 2, 1));
-    ArrayList<Integer> fifthBallot = new ArrayList<>(Arrays.asList(4, 3, 1, 2));
-    ArrayList<Integer> sixthBallot = new ArrayList<>(Arrays.asList(1, 2, 4, 3));
-
+    ArrayList<Integer> firstBallot = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+    ArrayList<Integer> secondBallot = new ArrayList<>(Arrays.asList(1, 0, 2, 4));
+    ArrayList<Integer> thirdBallot = new ArrayList<>(Arrays.asList(3, 2, 1, 0));
+    ArrayList<Integer> fourthBallot = new ArrayList<>(Arrays.asList(1, 0, 2, 3));
+    ArrayList<Integer> fifthBallot = new ArrayList<>(Arrays.asList(2, 1, 0, 3));
+    ArrayList<Integer> sixthBallot = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
 
     testBallots.add(firstBallot);
     testBallots.add(secondBallot);
@@ -33,22 +33,37 @@ public class TestIRV {
   // _QUESTION: Do we test private functions?? - Maybe not bc they are used in the public methods?
 
   // Testing runElection() only public method
+  // _QUESTION: what is printed to console? Election winner: name or just name?
+
   @Test
   public void testRunElection(){
     // Initialize IRV Election
     IRV hokageElection = intitializeTestIRV();
-    String hokage = "not a winner";
+
+    // Keep current System.out
+    PrintStream oldOut = System.out;
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+    // Change so System.out saved in baos
+    System.setOut(new PrintStream(baos));
 
     // Run election on irvElection
     try {
-      hokage = hokageElection.runElection();
+      hokageElection.runElection();
     }
     catch(IOException e) {
       e.printStackTrace();
     }
-    // String hokage = hokageElection.runElection();
+
+    // Reset the System.out to console
+    System.setOut(oldOut);
+
+    // baos contains winner printed from the runElection function
+    String output = new String(baos.toByteArray());
 
     // check if winner is as expected
-    assertEquals("Sasuke", hokage);
+    assertEquals("Sasuke", output);
+
+    System.out.println(output); // prints winner from function 
   }
 }
