94365749e6744434bd12cdf5617eff4220db39bf
Christine <tsaix223@umn.edu>
2018-11-07 12:45:37 -0600

    Add Auditor unit tests file

---
+++ Project1/src/TestAuditor.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+public class TestAuditor {
+	
+	// Testing auditProcess()
+	@Test
+	public void testAuditProcess() {
+
+	}
+
+	// Testing auditResult()
+	@Test
+	public void testAuditResult() {
+	
+	}
+
+	// Testing auditSetup()
+	@Test
+	public void testAuditSetup() {
+	
+	}
+
+	// Testing createAuditFile()
+	@Test
+	public void testCreateAuditFile() {
+	
+	}
+
+}
